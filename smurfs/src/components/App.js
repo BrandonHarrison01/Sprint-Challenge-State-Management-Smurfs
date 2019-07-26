783bc16cef4fb0b23344dac54c847b989c692b36
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-07-26 09:57:19 -0600

    successful get request and component tree layed out

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
+import { fetchSmurfs } from '../store/actions'
+
 import "./App.css";
+
 class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchSmurfs()
+  }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log('mapSTP: ', state)
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchSmurfs }
+)(App);
---
+++ smurfs/src/store/reducers/index.js
@@ -0,0 +1,21 @@
+import { FETCH_SMURF_START, FETCH_SMURF_SUCCESS, FETCH_SMURF_FAILURE } from '../actions'
+
+const initialState = {
+    error: '',
+    fetching: false,
+    smurfs: []
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_SMURF_START:
+            return {
+                ...state,
+                error: '',
+                fetching: true,
+                smurfs: []
+            }
+        default:
+            return state;
+    }
+}
